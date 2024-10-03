3b07ce1dca4b2575b0016fe4cfc18337e0023dbc
NourFagou <nourfagou@gmail.com>
2024-10-03 13:43:47 -0700

    mandatory

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,11 @@
-import signUpUser from './4-user-promise.js'; // Added .js extension
-import uploadPhoto from './5-photo-reject.js'; // Added .js extension
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason), // Ensure error is a string
-      }))
-    ));
+  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
 }
