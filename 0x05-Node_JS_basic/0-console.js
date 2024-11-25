f983324395f57ff37d199b72ca4d71b26056c745
NourFagou <nourfagou@gmail.com>
2024-11-25 17:38:38 +0000

    mandatory

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Noureddine Fagou <https://github.com/NourFagou>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
